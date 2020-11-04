4666ad313eeab1b8b03622b6f0601544cad0deb4
Oisin Ivory <47002132+Oisin-Ivory@users.noreply.github.com>
2020-11-04 13:33:08 +0000

    Refactor add favorites feature

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -2,18 +2,20 @@ import React, { useContext } from "react";
 import PageTemplate from '../components/templateMovieListPage'
 import {MoviesContext} from '../contexts/moviesContext'
 import AddToFavoritesButton from '../components/buttons/addToFavorites'
-
 const MovieListPage = () => {
   const context = useContext(MoviesContext);
+  const movies = context.movies.filter((m) => {  // New
+    return !("favorite" in m);
+  });
 
   return (
-      <PageTemplate 
-        title='No. Movies'
-        movies={context.movies}
-        action={(movie) => {
-          return <AddToFavoritesButton movie={movie} /> 
-        }}
-      />
+    <PageTemplate
+      title="No. Movies"
+      movies={movies}  /* Changed */
+      action={(movie) => {
+        return <AddToFavoritesButton movie={movie} />;
+      }}
+    />
   );
 };
 
